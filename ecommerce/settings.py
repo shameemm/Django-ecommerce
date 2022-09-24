8fef4070f3accf02992fafb0703a5f8106539bb1
shameemm <mandayappuramshameem@gmail.com>
2022-09-24 19:24:41 +0530

    signup_design

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project',
+        'USER': 'postgres',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
+        'PORT' : '5433',
     }
 }
 
@@ -115,6 +119,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATIC_URL = 'static/'
 
--- user/views.py
+++ user/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def home(request):
+    return render(request, 'home.html')
+
+def login(request):
     return render(request, 'login.html')
+
+def signup(request):
+    if request.method=='POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        phone = request.POST['phone']
+        email= request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        print(password)
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'Username Taken')
+            return redirect('signup')
+        elif User.objects.filter(email=email).exists():
+            messages.info(request, 'Email Exists')
+            return redirect('signup')
+        else:
+            # print(password)
+            user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name, email=email,phone=phone )
+            user.save()
+            print('success')
+            return redirect(to='login')
+    else:
+        return render(request, 'signup.html')
