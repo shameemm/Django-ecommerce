73079b9520a08525e14ac94f40becbf0ad48177a
shameemm <mandayappuramshameem@gmail.com>
2022-09-25 13:19:38 +0530

    normal-authentication-completed

--- user/views.py
+++ user/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.models import User, auth
+# from .models import Profile
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(password)
+        user=auth.authenticate(username=username, password=password)
+        if user is not None:
+            return redirect('home')
+        else:
+            return redirect('login')
+    else:   
+        return render(request, 'login.html')
 
 def signup(request):
     if request.method=='POST':
@@ -25,8 +38,9 @@ def signup(request):
             messages.info(request, 'Email Exists')
             return redirect('signup')
         else:
-            # print(password)
+            print(password)
             user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name, email=email,phone=phone )
+            # user=Profile.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name, email=email,phone=phone )
             user.save()
             print('success')
             return redirect(to='login')
